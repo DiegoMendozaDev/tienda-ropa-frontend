17b036e5c6e4735b2b857becadaaa4de201abf6b
DiegoMendozaDev <you@example.com>
2025-03-27 18:28:56 +0100

    enrutamiento

---
+++ src/pages/Chico.tsx
@@ -0,0 +1,8 @@
+function Chico(){
+    return (
+        <div>
+            <h1>Tienda Chicos</h1>
+        </div>
+    );
+}
+export default Chico;
---
+++ src/pages/Inicio.tsx
@@ -0,0 +1,8 @@
+function Inicio(){
+    return (
+        <div>
+            <h1>INICIO</h1>
+        </div>
+    );
+}
+export default Inicio;
