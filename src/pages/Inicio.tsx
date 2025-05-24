a97702b869404d27b0fb003dec779fd3ccf5727c
DiegoMendozaDev <diegomendozarosero@gmail.com>
2025-05-24 11:01:26 +0200

    cositas

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,20 +1,22 @@
 import NavScroll from "../components/NavScroll.tsx";
 import Carrusel from "../components/Carrusel.tsx";
 import ProductCards, { Product } from "../components/Productos.tsx";
-
+import { useState } from "react";
 
 function Inicio() {
+    const [search, setSearch] = useState('');
     const handleAddToCart = (product: Product) => {
         // Lógica para añadir producto al carrito
         alert(`Has añadido ${product.nombre} al carrito.`);
     };
     return (
         <div style={{ paddingTop: '100px' }}>
-            <NavScroll />
+            <NavScroll onSearchChange={setSearch}/>
             <Carrusel />
             <ProductCards 
                 url="https://127.0.0.1:8000/api/productos/ver"
                 onAddToCart={handleAddToCart}
+                search={search}
             />
             <h1>INICIO</h1>
         </div>
